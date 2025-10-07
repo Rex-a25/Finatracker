fb1182c8dbcd56cdf353382c21c22953725700d9
Rex-a25 <adeyehunboluwatife@gmail.com>
2025-10-07 11:54:58 +0100

    no comment

--- backend/server.js
+++ backend/server.js
@@ -500,3 +500,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
     console.log(` Server running on http://localhost:${PORT}`)
 );
+app.get("/api/test", (req, res) => res.json({ msg: "Server running!" }));
+
+export default app;
